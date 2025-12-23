85c60a6b8e7c10079a4191747604a63f8e5a3d42
AbhinavAnand241201 <iabhinavanand10@gmail.com>
2025-12-23 12:20:07 +0530

    Initial Commit

---
+++ PodFlowApp/PodFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodFlowAppApp.swift
+//  PodFlowApp
+//
+//  Created by ABHINAV ANAND  on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PodFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
